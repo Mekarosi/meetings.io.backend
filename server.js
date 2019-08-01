5862b69638822b72659cf85241c3fbb75bca8d98
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-08-01 20:39:51 +0100

    Installed mongoose

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const mongo= require("mongoose");
 const PORT = 5050;
 
 
 app.use(express.json());
+
+mongo.connect("mongodb+srv://mekarosi:caroline2411%@cluster0-jiqvx.mongodb.net/test?retryWrites=true&w=majority", err => {
+    if (err) {
+        console.log("BROKEN");
+       
+       } else {
+        console.log("CONNECTED");
+       }  
+});
 
 app.listen(PORT, err => {
     if (err) {
